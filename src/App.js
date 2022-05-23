e2d5217dbe2a1f7e8fb3df01214041a2b380de2f
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-05-24 01:23:54 +0600

    create extra folder for routes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+
 import Header from "./Components/Header/Header";
-import RequireAuth from "./Components/RequireAuth/RequireAuth";
-import AllProducts from "./Pages/AllProducts/AllProducts";
-import AddProduct from "./Pages/DashBoard/AddProduct/AddProduct";
-import AddReview from "./Pages/DashBoard/AddReview/AddReview";
-import DashBoard from "./Pages/DashBoard/DashBoard";
-import ManageOrder from "./Pages/DashBoard/ManageOrder/ManageOrder";
-import Order from "./Pages/DashBoard/Order/Order";
-import Profile from "./Pages/DashBoard/Profile/Profile";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import PurchaseProduct from "./Pages/PurchaseProduct/PurchaseProduct";
-import Register from "./Pages/Register/Register";
+import AppRoutes from "./Routes/Routes";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/allProducts" element={<AllProducts />} />
-        <Route
-          path="/dashboard"
-          element={
-            <RequireAuth>
-              <DashBoard />
-            </RequireAuth>
-          }
-        >
-          <Route index element={<Profile />} />
-          <Route path="addreview" element={<AddReview />} />
-          <Route path="order" element={<Order />} />
-          <Route path="manageOrder" element={<ManageOrder />} />
-          <Route path="addproduct" element={<AddProduct />} />
-        </Route>
-        <Route
-          path="/purchaseItem"
-          element={
-            <RequireAuth>
-              <PurchaseProduct />
-            </RequireAuth>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AppRoutes />
     </>
   );
 };
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import RequireAuth from "../Components/RequireAuth/RequireAuth";
+import AllProducts from "../Pages/AllProducts/AllProducts";
+import AddProduct from "../Pages/DashBoard/AddProduct/AddProduct";
+import AddReview from "../Pages/DashBoard/AddReview/AddReview";
+import DashBoard from "../Pages/DashBoard/DashBoard";
+import ManageOrder from "../Pages/DashBoard/ManageOrder/ManageOrder";
+import Order from "../Pages/DashBoard/Order/Order";
+import Profile from "../Pages/DashBoard/Profile/Profile";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import PurchaseProduct from "../Pages/PurchaseProduct/PurchaseProduct";
+import Register from "../Pages/Register/Register";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/allProducts" element={<AllProducts />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <DashBoard />
+            </RequireAuth>
+          }
+        >
+          <Route index element={<Profile />} />
+          <Route path="addreview" element={<AddReview />} />
+          <Route path="order" element={<Order />} />
+          <Route path="manageOrder" element={<ManageOrder />} />
+          <Route path="addproduct" element={<AddProduct />} />
+        </Route>
+        <Route
+          path="/purchaseItem"
+          element={
+            <RequireAuth>
+              <PurchaseProduct />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
